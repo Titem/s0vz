0beb3ef30c818e336e65a6c2c42f8208744648ab
noxbox <tim@buschmann-ge.de>
2015-02-26 09:24:16 +0100

    alert Funktion angepasst

--- s0vz-pig.c
+++ s0vz-pig.c
@@ -9,14 +9,21 @@ int main(int argc, char *argv[])
 
 	gpioSetAlertFunc(24, alert); /* call alert when LDR changes state */
 	
+	while(0)
+	{
+		gpioDelay(10000);
+	}
 	return 0;
 }
 
 void alert(int pin, int level, uint32_t tick)
 {
-	printf("/**********************/\n");
-	printf("Pin:\t%u\n", pin);
-	printf("Level\t:%u\n", level);
-	printf("Tick:\t%u\n", tick);
-	printf("/**********************/\n");
+	if(level == 0) 
+	{
+		printf("/**********************/\n");
+		printf("Pin:\t%u\n", pin);
+		printf("Level:\t%u\n", level);
+		printf("Tick:\t%u\n", tick);
+		printf("/**********************/\n");
+	}
 }
